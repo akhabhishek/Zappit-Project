2fde604727d66af0998bd98d98457af0f90851ac
Abhishek <akh.abhishek@gmail.com>
2021-03-04 22:04:05 +0530

    Added models, migrations, serializers, urls and templates

--- zappit/posts/views.py
+++ zappit/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class IndexView(TemplateView):
+    template_name = "posts/index.html"
